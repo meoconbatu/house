97add0f919a42bc2d93d7512e2bc49a5b41edd87
Thanh Mi Nguyen Thanh <thanhmi1111@live.com>
2017-02-22 00:49:55 +0700

    format code

--- house.go
+++ house.go
@@ -2,7 +2,7 @@ package house
 
 const testVersion = 1
 
-var rhymes = []string {
+var rhymes = []string{
 	" the malt\nthat lay in",
 	" the rat\nthat ate",
 	" the cat\nthat killed",
@@ -15,23 +15,24 @@ var rhymes = []string {
 	" the farmer sowing his corn\nthat kept",
 	" the horse and the hound and the horn\nthat belonged to",
 }
-func Song () string {
-	var result string 
-	for i:= 0; i<= len(rhymes);i++{
-		result += Verse(i+1)
+
+func Song() string {
+	var result string
+	for i := 0; i <= len(rhymes); i++ {
+		result += Verse(i + 1)
 		if i < len(rhymes) {
 			result += "\n\n"
 		}
 	}
 	return result
 }
-func Verse (n int) string {
-	return "This is" + Rhyme(n) + " the house that Jack built." 
+func Verse(n int) string {
+	return "This is" + Rhyme(n) + " the house that Jack built."
 }
 
 func Rhyme(n int) string {
 	if n == 1 {
-                return ""
-        }
-        return rhymes[n-2] + Rhyme(n-1)
+		return ""
+	}
+	return rhymes[n-2] + Rhyme(n-1)
 }
